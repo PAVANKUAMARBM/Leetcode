8d6ae1e354b6e926f0e470b045bb3f3049efd090
PAVANKUMAR B M <thenameispavan029@gmail.com>
2026-08-06 10:17:42 +0530

    Time: 4 ms (22.78%) | Memory: 12.4 MB (18.99%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
@@ -1,12 +1,25 @@
-class Solution:
-    def product(self, n):
-        prod = 1
-        while n:
-            prod *= n % 10
-            n //= 10
-        return prod
+# class Solution:
+#     def product(self, n):
+#         prod = 1
+#         while n:
+#             prod *= n % 10
+#             n //= 10
+#         return prod
 
+#     def smallestNumber(self, n, t):
+#         while self.product(n) % t != 0:
+#             n += 1
+#         return n
+
+
+class Solution:
     def smallestNumber(self, n, t):
-        while self.product(n) % t != 0:
-            n += 1
-        return n
+        while True:
+            product=1
+            for num in str(n):
+                product*=int(num)
+
+            if product%t==0:
+                return n
+            n+=1
+        
